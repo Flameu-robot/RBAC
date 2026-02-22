b67a07be655603754b8d689bdaae05f38845383c
Flameu-robot <nicksh.04@mail.ru>
2026-02-22 15:49:27 +0700

    2.4 Создал классы с фабриками для сортировки

---
+++ RBAC/src/main/java/org/example/sorter/AssignmentSorters.java
@@ -0,0 +1,21 @@
+package org.example.sorter;
+
+import org.example.assignment.RoleAssignment;
+import java.util.Comparator;
+
+public class AssignmentSorters {
+
+    private AssignmentSorters() {}
+
+    public static Comparator<RoleAssignment> byUsername() {
+        return Comparator.comparing(a -> a.user().username());
+    }
+
+    public static Comparator<RoleAssignment> byRoleName() {
+        return Comparator.comparing(a -> a.role().getName());
+    }
+
+    public static Comparator<RoleAssignment> byAssignmentDate() {
+        return Comparator.comparing(a -> a.metadata().assignedAt());
+    }
+}
